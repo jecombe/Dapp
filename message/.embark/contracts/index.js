18070afa904a74fd4986c5ed866b7c5516949990
Jeremy Combe <jeremcombe@hotmail.fr>
2018-10-24 04:59:09 +0200

    message dapp ethereum

---
+++ message/.embark/contracts/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+"ENS": require('./ENS').default,
+"Message": require('./Message').default,
+"ENSRegistry": require('./ENSRegistry').default,
+"Resolver": require('./Resolver').default,
+"FIFSRegistrar": require('./FIFSRegistrar').default
+}
---
+++ message/contracts/Message.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.0;
+
+contract  Message {
+
+    address public owner;
+    string public s;
+    uint public price = 0;
+
+    constructor (string _s) public {
+            owner = msg.sender;
+            s = _s;
+}
+
+    function set_message (string _mess) public payable {
+        require (msg.value > price);
+        s = _mess;
+        price  = msg.value;
+        owner = msg.sender;
+}
+
+    function get_message_and_price () public view returns (string, uint) {
+        return (s, price);
+    }
+}
